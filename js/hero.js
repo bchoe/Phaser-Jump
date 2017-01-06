148812c003b3856a96857574057d4386c91fa6d1
Bryan Choe <bryan.choe88@gmail.com>
2017-01-05 21:05:11 -1000

    lets see if this works

--- js/hero.js
+++ js/hero.js
@@ -40,6 +40,47 @@
       //referenced from update function
       this.sprite.update = this.update.bind(this);
     }
-  }
+
+    update(){
+      let hitPlatform = this.game.physics.arcade.collide(this.sprite, Game.platformsGroup);
+      let jumping = this.sprite.body.velocity.y !== 0;
+      //let go keyboard makes them stop moving
+      this.sprite.body.velocity.x = 0;
+      if (Game.cursors.left.isDown) {
+        this.sprite.body.velocity.x = -MOVE_SPEED;
+        if(!jumping && this.sprite.animations.currentAnim !== this.animations.left){
+          this.animations.left.play(ANIMATIONS.LEFT_SPEED, true);
+        }
+      } else if (Game.cursors.right.isDown) {
+        this.sprite.body.velocity.x = MOVE_SPEED;
+        if(!jumping && this.sprite.animations.currentAnim !== this.animations.right){
+          this.animations.right.play(ANIMATIONS.RIGHT_SPEED, true);
+        }
+      } else if(!jumping){
+        //  Stand still
+        if(this.sprite.animations.currentAnim !== this.animations.idle){
+          this.animations.idle.play(ANIMATIONS.IDLE_SPEED, true);
+        }
+      }
+
+      //  Allow the player to jump if they are touching the ground.
+      if (Game.cursors.up.isDown && this.sprite.body.touching.down && hitPlatform)
+      {
+        this.sprite.body.velocity.y = -JUMP_VELOCITY;
+        if(this.sprite.animations.currentAnim !== this.animations.jump){
+          this.animations.jump.play(ANIMATIONS.JUMP_SPEED, false);
+        }
+      }
+
+      // wrap around stage
+      if(this.sprite.x > CFG.GAME_WIDTH){
+        this.sprite.x = -this.sprite.width;
+      } else if(this.sprite.x < -this.sprite.width){
+        this.sprite.x = CFG.GAME_WIDTH;
+      }
+
+    }
+
+  };
 
 })(window.Phaser, window.Game, window.Game.Configuration);
